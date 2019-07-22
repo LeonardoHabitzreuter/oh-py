7df879401db02b736835f2329cc1615e949087f9
leonardohabitzreuter <leonardo.habitzreuter@br.experian.com>
2019-07-22 20:28:53 -0300

    some comments

--- garden/garden.py
+++ garden/garden.py
@@ -8,17 +8,21 @@ class Garden:
     toContinue = True
     while toContinue:
       for tree in self.trees:
+        # takeLast
         height = tree['alturas'][-1]
         newHeight = height + tree['fator']
         tree['alturas'].append(newHeight)
         if newHeight > tree['limite']:
           toContinue = False
+        # zfill fixa 5 caracters, eg 4 -> 00004
         tmp = format(height, ".2f").zfill(5)
         print(tmp, ">", tree['limite'], "? | ", end="")
+      # pula linha
       print()
     print("Final heights reached")
 
   def show(self):
     for tree in self.trees:
       height = tree['alturas'][-1]
+      # 2f = arredonda 2 casas decimais
       print(tree['especie'], ":", "{0:.2f}".format(height))
