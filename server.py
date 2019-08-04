643ca3bc420fb1d88c5d9550f1c75ca5c962cb6a
leonardohabitzreuter <leonardo.habitzreuter@br.experian.com>
2019-08-04 13:40:58 -0300

    init py in subfolders

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from src.users.service import UsersService
 
 app = Flask(__name__)
 
-@app.route("/users")
-def hello():
-  return jsonify([{ 'name': 'Jhon', 'age': 25 }, { 'name': 'Mary', 'age': 23 }])
+usersService = UsersService()
+
+@app.route('/users', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def users():
+  if request.method == 'GET':
+    return jsonify(usersService.get())
+  else:
+    return ''
---
+++ src/users/service.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+class UsersService:
+  def get(self):
+    return [{ 'name': 'Jhon', 'age': 25 }, { 'name': 'Mary', 'age': 23 }]
